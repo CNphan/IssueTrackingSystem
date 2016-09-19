03991b79b6bda3b6c8187199acf1dedfba736245
Christie Phan <christie@restorationmedia.com>
2016-09-19 16:47:43 -0700

    Finalize UI

---
+++ http-server/public/js/functions.js
@@ -0,0 +1,20 @@
+//Alert box Script
+function ticketConfirmation(){
+    alert("{Ticket Name has been created!}")
+}
+
+
+//Search ListOfDefects 
+
+var $rows = $('#tbody tr');
+$('#search').keyup(function() {
+    
+    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+    
+    $rows.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
+    }).hide();
+});
--- http-server/public/js/router.js
+++ http-server/public/js/router.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
 	$('.header').load("views/header.html");
 	$('.footer').load("views/footer.html");
 	$('.createDefects').load("views/createDefects.html");
-	$('.content1').load("views/content.html");
-	$('.content2').load("views/content2.html");
+	$('.defectsList').load("views/defectsList.html");
 });
